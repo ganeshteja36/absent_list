ca930eb952776b18788a8e789acc3dba9eda027d
ganeshteja36 <57834999+ganeshteja36@users.noreply.github.com>
2020-03-16 15:53:08 +0530

    Create Absent.py

---
+++ Absent.py
@@ -0,0 +1,31 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+class college:  
+
+    def __init__(self, name, absent_name):  
+
+        self.name = name  
+
+        self.absent_name = absent_name
+
+   
+# creating list        
+
+list = []  
+
+  
+# appending instances to list  
+
+list.append( college('Akash', 'ramesh') ) 
+
+list.append( college('Deependra', 'radha') ) 
+
+list.append( college('Raaper','ganesh') ) 
+
+  
+
+for obj in list: 
+
+    print( obj.absent_name) 
+
+  
